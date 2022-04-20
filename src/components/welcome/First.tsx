aeeb8f876aa3e557a423b1664383dc6f23eb8af5
ReeseKenn <yanbaorui1997@163.com>
2022-04-20 14:55:05 +0800

    第一个欢迎页面初步编写

--- src/components/welcome/First.tsx
+++ src/components/welcome/First.tsx
@@ -2,6 +2,8 @@ import { defineComponent } from 'vue';
 import pig from '../../assets/icons/pig.svg';
 import s from './First.module.scss';
 import { RouterLink } from 'vue-router';
+import divide from "../../assets/icons/divide.svg";
+
 export const First = defineComponent({
   setup: (props, context) => {
     return () => (
@@ -11,9 +13,10 @@ export const First = defineComponent({
           <h2>随时随地，想记就记</h2>
         </div>
         <div class={s.actions}>
-          <RouterLink class={s.fake} to="/start" >跳过</RouterLink>
-          <RouterLink to="/welcome/2" >下一页</RouterLink>
-          <RouterLink to="/start" >跳过</RouterLink>
+          <RouterLink to="/start"><img src={divide} style="color:0A3D62;width: 36px;" /></RouterLink>
+          <RouterLink to="/welcome/2"><img src={divide} style="width: 36px;" /></RouterLink>
+          <RouterLink to="/start"><img src={divide} style="width: 36px;" /></RouterLink>
+          <RouterLink to="/start"><img src={divide} style="width: 36px;" /></RouterLink>
         </div>
       </div>
     )
