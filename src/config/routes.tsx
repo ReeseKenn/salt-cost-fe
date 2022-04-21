0b82cc54c8d0f01e346d459b2989889a3089028e
ReeseKenn <yanbaorui1997@163.com>
2022-04-21 18:41:38 +0800

    添加startPage和Button组件

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -6,18 +6,20 @@ import { Third } from "../components/welcome/Third";
 import { Bar } from "../views/Bar";
 import { Foo } from "../views/Foo";
 import { Welcome } from "../views/Welcome";
+import { StartPage } from "../views/StartPage";
 
 export const routes: RouteRecordRaw[] = [
-  { path: '/', component: Foo },
-  { path: '/about', component: Bar },
+  {path: '/', component: Foo},
+  {path: '/about', component: Bar},
   {
     path: '/welcome',
     component: Welcome,
     children: [
-      { path: '1', component: First, },
-      { path: '2', component: Second, },
-      { path: '3', component: Third, },
-      { path: '4', component: Forth, },
+      {path: '1', component: First,},
+      {path: '2', component: Second,},
+      {path: '3', component: Third,},
+      {path: '4', component: Forth,},
     ]
-  }
+  },
+  {path: '/start', component: StartPage},
 ]
