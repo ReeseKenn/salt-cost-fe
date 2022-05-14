dfef94f672926abc67f29e89cf78280715f596e4
Reese Kenn <yanbaorui1997@163.com>
2022-05-14 20:15:51 +0800

    准备实现validate表单校验

--- src/components/tag/TagCreate.tsx
+++ src/components/tag/TagCreate.tsx
@@ -1,4 +1,4 @@
-import {defineComponent, PropType, reactive} from 'vue';
+import {defineComponent, PropType, reactive, toRaw} from 'vue';
 import s from './TagCreate.module.scss';
 import {MainLayout} from "../../layouts/MainLayout";
 import {Icon} from "../../shared/Icon";
@@ -17,12 +17,27 @@ export const TagCreate = defineComponent({
       name: '',
       sign: ''
     })
+    const onSubmit = (e: Event) => {
+      console.log(toRaw(formData))
+      // const rules = [
+      //   {key: 'name', required: true, message: '必填'},
+      //   {key: 'name', pattern: /^.{1,4}/, message: '只能填1到4个字符'},
+      //   {key: 'sign', required: true, message: '必填'}
+      // ]
+      // const errors = validate(formData,rules)
+      // errors = {
+      //   name: ['error1', 'error2'],
+      //   sign: ['error3', 'error4']
+      // }
+      e.preventDefault()
+
+    }
     return () => (
       <MainLayout>{{
         title: () => '新建标签',
         icon: () => <RouterLink to="/items/create"><Icon name="left" class={s.navIcon}/></RouterLink>,
         default: () => (
-          <form class={s.form}>
+          <form class={s.form} onSubmit={onSubmit}>
             <div class={s.formRow}>
               <label class={s.formLabel}>
                 <span class={s.formItem_name}>标签名</span>
@@ -38,7 +53,7 @@ export const TagCreate = defineComponent({
               <label class={s.formLabel}>
                 <span class={s.formItem_name}>符号 {formData.sign}</span>
                 <div class={s.formItem_value}>
-                  <EmojiSelect v-model={formData.sign} class={[s.formItem, s.emojiList, s.error]} />
+                  <EmojiSelect v-model={formData.sign} class={[s.formItem, s.emojiList, s.error]}/>
                 </div>
                 <div class={s.formItem_errorHint}>
                   <span>必填</span>
