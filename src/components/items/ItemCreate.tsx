e8a0a200256e69b767278fa2fd10f41ccb4359d1
Reese Kenn <yanbaorui1997@163.com>
2022-05-16 23:52:26 +0800

    初步添加收入支出类型列表

--- src/components/items/ItemCreate.tsx
+++ src/components/items/ItemCreate.tsx
@@ -13,6 +13,16 @@ export const ItemCreate = defineComponent({
   },
   setup: (props, context) => {
     const refKind = ref('支出')
+    const refExpensesTags = ref([
+        {id:1, name:'吃饭', sign:'￥',category:'expenses'},
+        {id:2, name:'交通', sign:'￥',category:'expenses'},
+        {id:3, name:'日用', sign:'￥',category:'expenses'},
+        {id:4, name:'DRS', sign:'￥',category:'expenses'}
+    ])
+    const refIncomeTags = ref([
+      {id:11, name:'薪水', sign:'￥',category:'income'},
+      {id:12, name:'其他', sign:'￥',category:'income'}
+    ])
     return () => (
       <MainLayout>{
         {
@@ -21,8 +31,12 @@ export const ItemCreate = defineComponent({
           default: () => <>
             <Tabs v-model:selected={refKind.value}>
               {/*<Tabs selected={refKind.value} onUpdateSelected={name => refKind.value = name}>*/}
-              <Tab name="支出">支出列表</Tab>
-              <Tab name="收入">收入列表</Tab>
+              <Tab name="支出">{refExpensesTags.value.map(tag=>{
+                return <span>{tag.name}</span>
+              })}</Tab>
+              <Tab name="收入">{refIncomeTags.value.map(tag=>{
+                return <span>{tag.name}</span>
+              })}</Tab>
             </Tabs>
             <div class={s.inputPad_wrapper}>
               <InputPad/>
