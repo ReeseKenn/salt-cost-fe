4d2fb3c091326bd544a96ca23d587192ecdb02ea
Reese Kenn <yanbaorui1997@163.com>
2022-05-18 23:54:23 +0800

    wip: 创建tagedit.tsx

--- src/components/tag/TagCreate.tsx
+++ src/components/tag/TagCreate.tsx
@@ -1,5 +1,5 @@
-import {defineComponent, PropType, reactive} from 'vue';
-import s from './TagCreate.module.scss';
+import {defineComponent, reactive} from 'vue';
+import s from './Tag.module.scss';
 import {MainLayout} from "../../layouts/MainLayout";
 import {Icon} from "../../shared/Icon";
 import {RouterLink} from "vue-router";
@@ -8,11 +8,6 @@ import {EmojiSelect} from "../../shared/EmojiSelect";
 import {Rules, validate} from "../../shared/validate";
 
 export const TagCreate = defineComponent({
-  props: {
-    name: {
-      type: String as PropType<string>
-    }
-  },
   setup: (props, context) => {
     const formData = reactive({
       name: '',
