6667219779e2ea4d9fddd336eef300653a24cc93
Reese Kenn <yanbaorui1997@163.com>
2022-05-19 20:33:49 +0800

    完成 TagEdit 页面的样式

--- src/components/tag/TagEdit.tsx
+++ src/components/tag/TagEdit.tsx
@@ -4,8 +4,8 @@ import {Rules, validate} from "../../shared/validate";
 import {MainLayout} from "../../layouts/MainLayout";
 import {RouterLink} from "vue-router";
 import {Icon} from "../../shared/Icon";
-import {EmojiSelect} from "../../shared/EmojiSelect";
 import {Button} from "../../shared/Button";
+import {TagForm} from "./TagForm";
 export const TagEdit = defineComponent({
   setup: (props, context) => {
     const formData = reactive({
@@ -31,39 +31,13 @@ export const TagEdit = defineComponent({
       <MainLayout>{{
         title: () => '新建标签',
         icon: () => <RouterLink to="/items/create"><Icon name="left" class={s.navIcon}/></RouterLink>,
-        default: () => (
-          <form class={s.form} onSubmit={onSubmit}>
-            <div class={s.formRow}>
-              <label class={s.formLabel}>
-                <span class={s.formItem_name}>标签名</span>
-                <div class={s.formItem_value}>
-                  <input v-model={formData.name} class={[s.formItem, s.input, s.error]}/>
-                </div>
-                <div class={s.formItem_errorHint}>
-                  <span>{errors['name']?errors['name']?.[0]: '　'}</span>
-                </div>
-              </label>
-            </div>
-            <div class={s.formRow}>
-              <label class={s.formLabel}>
-                <span class={s.formItem_name}>符号 {formData.sign}</span>
-                <div class={s.formItem_value}>
-                  <EmojiSelect v-model={formData.sign} class={[s.formItem, s.emojiList, s.error]}/>
-                </div>
-                <div class={s.formItem_errorHint}>
-                  <span>{errors['sign']?errors['sign']?.[0]: '　'}</span>
-                </div>
-              </label>
-            </div>
-            <p class={s.tips}>记账时长按标签即可进行编辑</p>
-            <div class={s.formRow}>
-              <div class={s.formItem_value}>
-                <Button class={[s.formItem, s.button]}>确定</Button>
-              </div>
-            </div>
-
-          </form>
-        )
+        default: () => <>
+          <TagForm />
+          <div class={s.actions}>
+            <Button level='danger' class={s.removeTags} onClick={() => { }}>删除标签</Button>
+            <Button level='danger' class={s.removeTagsAndItems} onClick={() => { }}>删除标签和记账</Button>
+          </div>
+        </>
       }}</MainLayout>
     )
   }
