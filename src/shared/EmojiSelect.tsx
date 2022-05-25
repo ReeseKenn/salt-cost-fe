43e076816527c4a8d7bb973fcc049adde90673b9
ReeseKenn <yanbaorui1997@163.com>
2022-05-25 16:42:08 +0800

    让 EmojiSelect 支持 v-model 和 props 两种事件形式

--- src/shared/EmojiSelect.tsx
+++ src/shared/EmojiSelect.tsx
@@ -1,47 +1,52 @@
-import {computed, defineComponent, ref} from 'vue';
+import { computed, defineComponent, PropType, ref } from 'vue';
 import s from './EmojiSelect.module.scss';
-import {emojiList} from "./emojiList";
+import { emojiList } from "./emojiList";
 
 export const EmojiSelect = defineComponent({
   props: {
     modelValue: {
       type: String,
+    },
+    onUpdateModelValue: {
+      type: Function as PropType<(emoji: string) => void>
     }
   },
   setup: (props, context) => {
     const refSelected = ref(0)
-    const table:[string,string[]][] = [
-      ['表情',['face-smiling', 'face-affection', 'face-tongue', 'face-hand', 'face-neutral-skeptical', 'face-sleepy',
-        'face-unwell', 'face-hat', 'face-glasses', 'face-concerned', 'face-negative', 'face-costume','emotion']],
-      ['手势',['hand-fingers-open', 'hand-fingers-partial', 'hand-single-finger', 'hand-fingers-closed', 'hands', 'hand-prop',
+    const table: [string, string[]][] = [
+      ['表情', ['face-smiling', 'face-affection', 'face-tongue', 'face-hand', 'face-neutral-skeptical', 'face-sleepy',
+        'face-unwell', 'face-hat', 'face-glasses', 'face-concerned', 'face-negative', 'face-costume', 'emotion']],
+      ['手势', ['hand-fingers-open', 'hand-fingers-partial', 'hand-single-finger', 'hand-fingers-closed', 'hands', 'hand-prop',
         'body-parts']],
-      ['职业',['person', 'person-gesture', 'person-role', 'person-fantasy', 'person-activity', 'person-sport', 'person-resting']],
-      ['衣服',['clothing']],
-      ['动物',['cat-face', 'monkey-face', 'animal-mammal', 'animal-bird', 'animal-amphibian', 'animal-reptile', 'animal-marine',
+      ['职业', ['person', 'person-gesture', 'person-role', 'person-fantasy', 'person-activity', 'person-sport', 'person-resting']],
+      ['衣服', ['clothing']],
+      ['动物', ['cat-face', 'monkey-face', 'animal-mammal', 'animal-bird', 'animal-amphibian', 'animal-reptile', 'animal-marine',
         'animal-bug']],
-      ['植物',['plant-flower', 'plant-other']],
-      ['自然',['sky & weather','science']],
-      ['食物',['food-fruit', 'food-vegetable', 'food-prepared', 'food-asian', 'food-marine', 'food-sweet', 'drink']],
-      ['运动',['sport', 'game']],
-      ['交通',['transport-ground', 'transport-water', 'transport-air']],
-      ['住宿',['hotel']]
+      ['植物', ['plant-flower', 'plant-other']],
+      ['自然', ['sky & weather', 'science']],
+      ['食物', ['food-fruit', 'food-vegetable', 'food-prepared', 'food-asian', 'food-marine', 'food-sweet', 'drink']],
+      ['运动', ['sport', 'game']],
+      ['交通', ['transport-ground', 'transport-water', 'transport-air']],
+      ['住宿', ['hotel']]
     ]
-    const emojis = computed(()=>table[refSelected.value][1].map(category=>
-      emojiList.find(item=>item[0]===category)?.[1].map(item=>
-        <li class={item===props.modelValue?s.selectedEmoji:''} onClick={()=>{onClickEmoji(item)}}>{item}</li>)
+    const emojis = computed(() => table[refSelected.value][1].map(category =>
+      emojiList.find(item => item[0] === category)?.[1].map(item =>
+        <li class={item === props.modelValue ? s.selectedEmoji : ''} onClick={() => {
+          onClickEmoji(item)
+        }}>{item}</li>)
     ))
-    const onClickTab = (index: number)=>{
+    const onClickTab = (index: number) => {
       refSelected.value = index
     }
-    const onClickEmoji = (emoji:string)=>{
+    const onClickEmoji = (emoji: string) => {
       context.emit('update:modelValue', emoji)
     }
     return () => (
       <div class={s.emojiList}>
         <nav>
-          {table.map((item,index)=>
-            <span class={index===refSelected.value?s.selected:''}
-            onClick={()=>onClickTab(index)}>{item[0]}</span>)}
+          {table.map((item, index) =>
+            <span class={index === refSelected.value ? s.selected : ''}
+                  onClick={() => onClickTab(index)}>{item[0]}</span>)}
         </nav>
         <ol>
           {emojis.value}
