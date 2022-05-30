9117aa3e385a9c848aabfa0a7aa9394e2e3fe801
Reese Kenn <yanbaorui1997@163.com>
2022-05-30 22:40:17 +0800

    bugfix

--- src/components/items/ItemList.tsx
+++ src/components/items/ItemList.tsx
@@ -79,7 +79,7 @@ export const ItemList = defineComponent({
                     <FormItem label='结束时间' v-model={customTime.end} type='date' />
                     <FormItem>
                       <div class={s.actions}>
-                        <button type="button">取消</button>
+                        <button type="button" onClick={() => refOverlayVisible.value = false}>取消</button>
                         <button type="submit">确认</button>
                       </div>
                     </FormItem>
