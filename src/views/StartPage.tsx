b5faf2fbe73dcf7e6a7bac6a9ae986f7d9eb7e12
Reese Kenn <yanbaorui1997@163.com>
2022-06-03 23:51:11 +0800

    startpage页面使用overlayicon组件

--- src/views/StartPage.tsx
+++ src/views/StartPage.tsx
@@ -1,24 +1,20 @@
-import {defineComponent, ref} from 'vue';
+import {defineComponent} from 'vue';
 import {Button} from '../shared/Button';
 import s from './StartPage.module.scss';
 import {FloatButton} from "../shared/FloatButton";
 import {Center} from "../shared/Center";
 import {Icon} from "../shared/Icon";
-import {Overlay} from "../shared/Overlay";
+import {OverlayIcon} from "../shared/Overlay";
 import {RouterLink} from "vue-router";
 import {MainLayout} from "../layouts/MainLayout";
 
 export const StartPage = defineComponent({
   setup: (props, context) => {
-    const refOverlayVisible = ref(false)
-    const onClickMenu = () => {
-      refOverlayVisible.value = !refOverlayVisible.value
-    }
     return () => (
       <MainLayout>{
         {
           title: ()=>'海盐账本',
-          icon: () => <div onClick={onClickMenu}><Icon name="menu" class={s.navIcon}/></div>,
+          icon: () => <OverlayIcon />,
           default: ()=><>
             <Center class={s.pig_wrapper}>
               <Icon name="pig" class={s.pig}/>
@@ -31,9 +27,6 @@ export const StartPage = defineComponent({
             <RouterLink to="/items/create">
               <FloatButton iconName="add"/>
             </RouterLink>
-            {refOverlayVisible.value &&
-            <Overlay onClose={() => refOverlayVisible.value = false}/>
-            }
           </>
         }
       }</MainLayout>
